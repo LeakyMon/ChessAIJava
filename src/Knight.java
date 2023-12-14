1fe5eac7271a12435c7987dd5d10cb87ae9f32b8
LeakyMon <hmonforte17@gmail.com>
2023-12-14 07:12:20 -0500

    Knight Complete

--- src/Knight.java
+++ src/Knight.java
@@ -6,6 +6,33 @@ public class Knight extends ChessPiece{
     }
 
 
-    public boolean isValidMove(int startRow, int startCol, int endRow, int endCol, ChessPiece[][] board) {return false;}
+    public boolean isValidMove(int startRow, int startCol, int endRow, int endCol, ChessPiece[][] board) {
+
+
+        int rowDiff = Math.abs(endRow - startRow);
+        int colDiff = Math.abs(endCol - startCol);
+
+        // Knights move in an L-shape: 2 squares in one direction and 1 square in the perpendicular direction
+        if ((rowDiff == 2 && colDiff == 1) || (rowDiff == 1 && colDiff == 2)) {
+            // Check for capturing an opponent's piece
+            if (board[endRow][endCol] != null && !board[endRow][endCol].getColor(board[endRow][endCol]).equals(this.color)) {
+                System.out.println("Enemy " + board[endRow][endCol].getType(board[endRow][endCol]) + " captured");
+                return true;
+            }
+            else if (board[endRow][endCol] != null && board[endRow][endCol].getColor(board[endRow][endCol]).equals(this.color)){
+                System.out.println("Cannot capture your own pieces");
+                return false;
+            }
+            return true; // Valid knight move
+        }
+
+        return false; // Invalid move for a knight
+
+
+
+
+
+
+    }
 
 }
